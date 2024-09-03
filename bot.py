f034d001f67f441565fcbe30b1b6d783ff693965
Role417 <157006183+Role417@users.noreply.github.com>
2024-09-03 22:59:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,7 +81,7 @@ class Bot(Client):
 
         app = web.AppRunner(await web_server())
         await app.setup()
-        bind_address = "0.0.0.0"
+        bind_address = "0.0.7.0"
         await web.TCPSite(app, bind_address, PORT).start() 
         await restart_index(self)
     
