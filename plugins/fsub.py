98511a03ab0eed65f9f08737ab3efb34e5162d4b
Role417 <157006183+Role417@users.noreply.github.com>
2024-09-03 13:25:08 +0530

    Update fsub.py

--- plugins/fsub.py
+++ plugins/fsub.py
@@ -179,7 +179,7 @@ async def send_file(client, query, ident, file_id):
                     )
     )
     replied = ok.id    
-    da = await message.reply(DELETE_TXT, reply_to_message_id=replied)
+    da = await query.reply(DELETE_TXT, reply_to_message_id=replied)
     await asyncio.sleep(30)
     await message.delete()    
     await da.delete()
