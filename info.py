d983aa3c810f1a4af3707e40f7929c4e46907ea2
Role417 <157006183+Role417@users.noreply.github.com>
2024-09-15 16:40:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,8 +27,8 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/39b86f6dc3e12c8976f2b.jpg h
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/bba1fba72bed143e8c8be.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/37af06680cbd13176c536.jpg")
 # Admins, Channels & Users
-OWNER_ID = environ.get('OWNER_ID', '1297128957 1509123054')
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1297128957 1509123054').split()]
+OWNER_ID = environ.get('OWNER_ID', '7425490417 1509123054')
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7425490417 1509123054').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
